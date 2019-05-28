67212acaf399533755e1dccb02c0fff5ab1d3f1d
Daryl Cimafranca <cimafrancad45@gmail.com>
2019-05-28 18:54:27 -0400

    Homework 5/28/2019 submission

--- starter-code/20190829/src/main/java/com/company/FormattedOutput.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class FormattedOutput {
-    public static void main(String[] args) {
-        System.out.format("My name is %s and I am %d years old.", "Daryl", 33);
-    }
-}
