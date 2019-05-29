cbb0a7fc0c566069a9deb7cdfc6eefe170396504
Daryl Cimafranca <cimafrancad45@gmail.com>
2019-05-29 14:01:49 -0400

    Initial Upload and Folder Update

---
+++ 20190830/U1M1L2/src/main/java/com/company/AddFive.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class AddFive {
+
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int num1 = Integer.parseInt(myScanner;
+        int sum = + 5;
+    }
+}
